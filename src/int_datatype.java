188fc749926df881510bd22bbc2bcd49fc680a95
Elizabeth Snead <ebs.my.sites@gmail.com>
2018-10-15 21:55:36 -0400

    learning about int

---
+++ src/int_datatype.java
@@ -0,0 +1,12 @@
+/*int is short for integer, which are all positive and negative numbers, including zero. 
+ * This number could represent the number of visits a website has received or the number of programming languages you know.
+ * The int data type only allows values between -2,147,483,648 and 2,147,483,647.
+ */
+
+public class int_datatype {
+	public static void main(String[] args) {
+
+		System.out.println(2323347);
+
+	}
+}
