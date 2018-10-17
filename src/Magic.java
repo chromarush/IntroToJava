015214856c6b1f9057931e45d8d3a9d2ecb77879
Elizabeth Snead <ebs.my.sites@gmail.com>
2018-10-16 20:50:34 -0400

    simple math exercise to learn to use int and print functions

---
+++ src/Magic.java
@@ -0,0 +1,16 @@
+//CodeAcademy Project
+public class Magic {
+	public static void main(String[] args) {
+
+		int myNumber = 12;
+    // myNumber is the original number
+    int stepOne = myNumber * myNumber;
+    int stepTwo = stepOne + myNumber;
+    int stepThree = stepTwo / myNumber;
+    int stepFour = stepThree + 17;
+    int stepFive = stepFour = myNumber;
+    int stepSix = stepFive / 6;
+    System.out.println(stepSix);
+
+	}
+}
