b64731bc9706f58de366469b4c0c7efb96f768dd
Elizabeth Snead <ebs.my.sites@gmail.com>
2018-10-19 17:46:39 -0400

    If contrl flow exercise

---
+++ src/controlflow.java
@@ -0,0 +1,11 @@
+public class controlflow {
+	public static void main (String[] args){
+		int x = 15;
+		if (x > 9) {
+			System.out.println("x = " + x);
+			x = x - 1;
+			System.out.println("x = " + x);
+		}
+		
+	}
+}
