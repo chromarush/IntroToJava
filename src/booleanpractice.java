77b98ddf942879b7e514f74af4efddbe86ec38bf
Elizabeth Snead <ebs.my.sites@gmail.com>
2018-10-18 22:51:55 -0400

    Learning while loops from hradfirst java pg 46

---
+++ src/booleanpractice.java
@@ -0,0 +1,14 @@
+public class booleanpractice{
+	
+	public static void main(String[] args) {
+		int x = 1;
+		System.out.println("Before the loop");
+		while (x <= 10) {
+			System.out.println("in the loop");
+			System.out.println("The value of x is " + x);
+			x = x + 1;
+		}
+		System.out.println("Thid is after the loop");
+		
+	}
+}
