bef8d0ff8bd5ac4b1654578ce245f5cc4ba69427
Elizabeth Snead <ebs.my.sites@gmail.com>
2018-10-20 21:29:44 -0400

    control flow with switch

---
+++ src/switchy.java
@@ -0,0 +1,27 @@
+public class switchy{
+	
+	public static void main (String[] args) {
+		
+		int appreview = 0;
+		
+		switch (appreview) {
+		
+		case 1: System.out.println("Terrible!");
+		break;
+		
+		case 2: System.out.println("Not worth the time.");
+		break;
+		
+		case 3: System.out.println("Ok.");
+		break;
+		
+		case 4:System.out.println("Good.");
+		break;
+		
+		case 5:System.out.println("GREAT!");
+		break;
+		
+		default: System.out.println("No rating");
+		}
+	}
+}
