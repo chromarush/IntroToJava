c93dc39ce7610200b3f6b4602878021abd6f2c78
Elizabeth Snead <ebs.my.sites@gmail.com>
2018-10-20 21:42:52 -0400

    switch statement for picking a continent

---
+++ src/continents.java
@@ -0,0 +1,34 @@
+public class continents {
+	public static void main(String[] args) {
+    //Program to spit out a continent
+    
+int continent = 4;
+    
+		switch (continent){
+        
+      case 1: System.out.println("North America: Mexico City, Mexico");
+      break;
+        
+      case 2: System.out.println("South America: Sao Paulo, Brazil");
+        break;
+        
+      case 3: System.out.println("Europe:Moscow, Russia");
+        break;
+        
+      case 4: System.out.println("Africa: Lagos, Nigeria");
+        break;
+        
+      case 5: System.out.println("Asia: Shanghai, China");
+        
+      case 6: System.out.println("Austrailia:Sydney, Austrailia");
+        break;
+        
+      case 7: System.out.println("Antarctica: McMurdo Station, US");
+        break;
+      
+      default: System.out.println("Undefined continent!");
+        break;
+    }
+
+	}
+}
